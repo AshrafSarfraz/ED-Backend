fdbef7a555808783041d3ff335cb11d3c07537a1
HalabSaudi <ashrafq952@gmail.com>
2026-06-19 16:56:38 +0300

    done

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.use("/api/buyer/orders",    require("./src/routes/buyer/buyerOrder"));
 app.use("/api/supplier/bids",   require("./src/routes/supplier/bids"));
 app.use("/api/supplier/orders", require("./src/routes/supplier/supplierOrder"));
 
-app.use("/api/buyer/payments",  require("./src/routes/payment"));
+app.use("/api/payments", require("./src/routes/payment"));
 
 
 app.use("/api/delivery/auth",   require("./src/routes/riderCompany/RiderAuth"));
